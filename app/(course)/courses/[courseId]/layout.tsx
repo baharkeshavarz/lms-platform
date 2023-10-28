420575bf1f3ec3e7ba685b7209dd00d9a621487f
BaharKeshavarz <bahar.keshavarzc@gmail.com>
2023-10-28 20:03:18 +0300

    list chapters

---
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -0,0 +1,64 @@
+import { getProgress } from '@/actions/get-progress';
+import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import React from 'react'
+import CourseSidebar from '../_components/course-sidebar';
+
+const CourseLayout = async({
+     children,
+     params
+    }: {
+      children:React.ReactNode,
+      params: { courseId: string }
+}) => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/")
+  }
+
+  // Find the course
+  const course = await db.course.findUnique({
+    where: {
+       id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        include: {
+          userProgress: {
+            where: {
+              userId
+            }
+          }
+        },
+        orderBy: {
+          position: "asc"
+        }
+      },
+    },
+   })
+
+  if (!course) {
+    return redirect("/")
+  }
+  const progressCount = await getProgress(userId, course.id)
+  
+  return (
+    <div className="h-full">
+       <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-0">
+           <CourseSidebar
+              course={course}
+              progressCount={progressCount}
+           />
+       </div>
+        <main className="md:pl-80 h-full">
+           {children}
+        </main>
+    </div>
+  )
+}
+
+export default CourseLayout
